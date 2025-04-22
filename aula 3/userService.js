342e61813a802b86a951c257137dff03c00dd93f
nicolasfsz <nicolas.desouza02062008@gmail.com>
2025-04-22 08:40:38 -0300

    mudanças 21/04

--- aula 3/userService.js
+++ aula 3/userService.js
@@ -7,43 +7,7 @@ const bcrypt = require('bcryptjs');
 
 
 class userService{
-    constructor(){
-        this.filePath = path.join(__dirname, 'user.json');
-        this.users = this.loadUsers(); //array para armazenar user
-        this.nextId = this.getNextId(); //contador para gerar id
-    }
-
-    loadUsers(){
-        try{           
-         if(fs.existsSync(this.filePath)){ //verifica se o arquivo existe
-            const data = fs.readFileSync(this.filePath); //le o arquivo 
-            return JSON.parse(data); //transforma o item em objeto
-            }
-        } catch (erro) {
-            console.log('erro ao carregar arquivo', erro);
-        }
-return [];
-    }
-
-    getNextId(){
-        try{
-        if(this.users.length===0) return 1;
-        return Math.max(...this.users.map(user => user.id))+1;
-        } catch (erro) {
-            console.log('erro ao buscar proximo', erro);
-        }
-    }
-
-    saveUsers(){
-        try{
-        fs.writeFileSync(this.filePath, JSON.stringify(this.users));
-    }catch (erro) {
-        console.log('erro ao salvar arquivo')
-    }
-        
-}
-
-    
+   
 
     async addUser(nome, email, senha, endereco, telefone, cpf){
         try{  
